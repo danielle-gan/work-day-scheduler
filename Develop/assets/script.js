c4d02948428e0bd5f667f62d6822ca9bde541169
danielle-gan <danielle.gan@uconn.edu>
2021-12-25 12:39:20 -0500

    added some icons and working on updating time cards

---
+++ Develop/assets/script.js
@@ -0,0 +1,27 @@
+// display date
+var date = new Date()
+
+function displayDate() {
+    $("#currentDay").html(moment(date).format('MMMM Do YYYY, h:mm a'));
+    setInterval(displayDate, 60*1000);
+};
+displayDate();
+
+// update color of time blocks based on the time
+function updateCards() {
+    currentHour = date.getHours();
+    for(let i=0; i<12;i++)
+        if (currentHour === document.querySelector(".id")) {
+            console.log(".id")
+        }
+        else {
+            console.log("not the current hour")
+        }
+};
+updateCards();
+
+//create local storage for tasks
+
+//add task when you click on a time card
+
+//save task when you click on the checkmark
