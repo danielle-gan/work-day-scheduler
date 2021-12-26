a1969c94f22cd835a4a4c62776173727844eb624
danielle-gan <danielle.gan@uconn.edu>
2021-12-26 16:24:52 -0500

    created local storage and saved tasks

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -3,16 +3,14 @@ var date = new Date()
 
 function displayDate() {
     $("#currentDay").html(moment(date).format('MMMM Do YYYY, h:mm a'));
-    setInterval(displayDate, 60*1000);
-};
-displayDate();
+}
+setInterval(displayDate(), 60*1000);
 
 // update color of time blocks based on the time
 function updateCards() {
     var currentHour = moment().hour();
     $(".time-block").each(function() {
         var timeBlock = $(this).attr("id");
-        console.log(timeBlock, currentHour);
         if (timeBlock < currentHour) {
             $(this).addClass("past");
         }
@@ -26,7 +24,25 @@ function updateCards() {
 };
 window.onload = updateCards();
 
-//create local storage for tasks
+//create and load local storage for tasks
+$("#7 .task").val(localStorage.getItem("7"));
+$("#8 .task").val(localStorage.getItem("8"));
+$("#9 .task").val(localStorage.getItem("9"));
+$("#10 .task").val(localStorage.getItem("10"));
+$("#11 .task").val(localStorage.getItem("11"));
+$("#12 .task").val(localStorage.getItem("12"));
+$("#13 .task").val(localStorage.getItem("13"));
+$("#14 .task").val(localStorage.getItem("14"));
+$("#15 .task").val(localStorage.getItem("15"));
+$("#16 .task").val(localStorage.getItem("16"));
+$("#17 .task").val(localStorage.getItem("17"));
+$("#18 .task").val(localStorage.getItem("18"));
 
+//save task
+$(".saveBtn").on("click", function () {
+    var task = $(this).siblings(".task").val();
+    var time = $(this).parent().attr("id");
 
-//save task when you click on the checkmark
+    //set items in local storage.
+    localStorage.setItem(time, task);
+});
